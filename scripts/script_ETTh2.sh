c380144d09fb4f079141ea951753a6f6ff83d128
youjjjj <98287935+youjjjj@users.noreply.github.com>
2024-04-06 14:36:56 +0200

    Update script_ETTh2.sh

--- scripts/script_ETTh2.sh
+++ scripts/script_ETTh2.sh
@@ -1,25 +1,31 @@
 # export CUDA_VISIBLE_DEVICES=2
 
+experiment_name=KUNetTest_20240405
+
+# input sequence length
+seq_len=720
+
+# model information
+model_name=NKUNet
+model_id_name=KUN
+
+# dataset information
+root_path_name=./dataset/ETT-small/
+data_path_name=ETTh2.csv
+data_name=ETTh2
+
+# make dir for logs
 if [ ! -d "./logs" ]; then
     mkdir ./logs
 fi
 
-if [ ! -d "./logs/KUNetTest" ]; then
-    mkdir ./logs/KUNetTest
+if [ ! -d "./logs/"$experiment_name ]; then
+    mkdir ./logs/$experiment_name
 fi
-seq_len=720
-model_name=KUNet
-
-
-
-root_path_name=./dataset/ETT-small/
-data_path_name=ETTh2.csv
-model_id_name=ETTh2
-data_name=ETTh2
- 
 
 random_seed=2021
 
+# Main experiments loop
 for pred_len in  96 192 336 720
 do
     python -u run_kun.py \
@@ -34,21 +40,17 @@ do
     --seq_len $seq_len \
     --label_len $seq_len \
     --pred_len $pred_len \
+                         \
     --enc_in 7 \
-    --e_layers 2 \
-    --n_heads 2 \
-    --d_model 16 \
-    --d_ff 32 \
     --dropout 0.3\
-    --fc_dropout 0.3\
-    --head_dropout 0\
-    --patch_len 16\
-    --stride 8\
     --des 'Exp' \
     --train_epochs 100\
     --patience 10 \
-    --itr 1 --batch_size 128 --learning_rate 0.0001 \
-    --lradj CARD --warmup_epochs 10 \
-    2>&1 | tee logs/KUNetTest/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log \
+    --itr 1 \
+    --batch_size 128 \
+    --learning_rate 0.0001 \
+    --lradj CARD \
+    
+    2>&1 | tee logs/$experiment_name/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log \
 
 done
